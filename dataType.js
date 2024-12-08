92f63fa031c6fc2a9306f77402c26e24ff7cbd62
akmanil <maharanaanil424@gmail.com>
2024-12-09 00:37:31 +0530

    adding data type js file

---
+++ dataType.js
@@ -0,0 +1,23 @@
+//let const var variables
+//for multicomments use/* ... */
+//string boolean number symbol object array are the data tpes
+
+
+/*Built-in object types can be:
+
+objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more.*/
+
+
+const obj = {
+    name:"Anil",
+    class:"B.Tech",
+    roll_no:"2002050013"
+};
+let arr =[1,2,3,4,5];
+let bigIntiger = BigInt("536173715362145274624247266724674682626477267")
+// for represting that type of large number 
+
+typeof obj;
+console.log(typeof obj);//tyeof used for knowing the type of data type
+//in javaScript with out value data types are called undefined
+
