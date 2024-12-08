272b5c0fe5d2eec5c288ea2f9806a9c28efddb2d
akmanil <maharanaanil424@gmail.com>
2024-12-09 00:20:07 +0530

    Adding document and windows pre build displayu possibility

---
+++ document.js
@@ -0,0 +1,11 @@
+function change() {
+     document.write("Hey its Write document");
+     window.alert("button was clicked");
+
+     
+}
+function Print() {
+      window.print() ;
+   //document.getElementById("print").innerHTML("Printed") 
+// }
+}
