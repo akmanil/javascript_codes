1a31d6eb2c30e1773f8d78ae52797ee2253e90ab
akmanil <maharanaanil424@gmail.com>
2024-12-09 22:53:53 +0530

    adding array itteration js files into my git repo

---
+++ array.js
@@ -0,0 +1,45 @@
+const arr =["anil" , "sunil" , "sipun" ,"saroj" ,"gudu" , "gugul"];
+//that above method for general 
+ const Name = new Array(1 ,2 ,3 ,4 ,5);//1,2,3,4,5 this are the arguments
+ //two methods of creating array
+//  console.log(arr , Name);
+//  console.log(arr[0]);
+//  const arrayElement = (){
+//     for (let i = 0; i < arr.length; i++) {
+//        return <li>array[i]</li>;
+        
+//     }
+//  }//that code should be write in the div of index after 
+//  console.log(arrayElement)
+
+//array iteration
+
+// let indeX = arr.forEach(function (value) {
+//     console.log(value);
+    
+// })
+// let index2 = Name.map((s) =>{
+//     console.log(s);
+// })
+// let ind = Name.map((s) =>{
+//     let val ;
+//     val = s+1;
+//     console.log(val);
+   
+  
+   
+// })
+// // console.log(ind)
+// let ar =[] ;
+// ar.push(ind)
+// console.log(ind);
+
+// Name.forEach((value ,index ,arrray)=> {
+//       arrray[index] = value +1
+// });
+// console.log(Name);
+let emp = []
+for (let i = 0; i < Name.length; i++) {
+    emp.push(Name[i]+1);
+    }
+console.log(emp);
--- event.js
+++ event.js
@@ -1,13 +1,22 @@
 // javaScript events are
-const btn = document.querySelector(".event")
+// const btn = document.querySelector(".event")
 
-function greet(event){
-    console.log("greet",event);
-}
-// btn.onclick = greet;
-btn.addEventListener("click" , greet);
+// function greet(event){
+//     console.log("greet",event);
+// }
+// // btn.onclick = greet;
+// btn.addEventListener("click" , greet);
 // or
 //  btn.addEventListener("click" ,(Event)={
 //     console.log("greet" , Event);
      
 //   });
+// nested array
+
+// const obj=[
+    
+//     {class:[B.Tech ,VSSUT , 2024]},
+//     {class:["12th" ,PRC , 2020]}
+// ];
+
+//Array itteration method
