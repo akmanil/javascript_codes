3d167d89965b59d45c605201bb2015c876993df3
akmanil <maharanaanil424@gmail.com>
2024-12-09 11:45:10 +0530

    Adding a js file which has oject method

---
+++ objMethod.js
@@ -0,0 +1,13 @@
+let person = {
+    firstName: "Anil" ,
+    lastName : "Maharana" ,
+    fullName :function Fullname() {
+        return person.firstName + "" + person.lastName
+        
+    }
+}
+
+// let myName = function Fullname() {
+//     return person.firstName + "" + person.lastName   
+// }
+// console.log(myName)
